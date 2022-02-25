a1b3bf05efa9ffd85e76f2895fdacec348de9f75
Rawan Mohamed <rawanmohamed2023@u.northwestern.edu>
2022-02-25 15:03:07 -0800

    Comment out base code in main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const parallax = document.getElementById('parallax');
+/*const parallax = document.getElementById('parallax');
 
 window.addEventListener('scroll', function() {
   let scroll = window.pageYOffset;
   parallax.style.backgroundPositionY = scroll * 0.7 + 'px';
-});
+});*/
