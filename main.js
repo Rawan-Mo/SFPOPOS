da240fd86162caf3f1eed29420d8bb50a8df5a50
Lauren Bichelmeir <73313614+lbichelmeir@users.noreply.github.com>
2022-03-07 12:52:07 -0800

    Final Version without Waypoint
    Final Version without Waypoint

--- main.js
+++ main.js
@@ -141,3 +141,12 @@ bridge.addEventListener("touchmove", function(e) {
         drawDot(brushPos.x, brushPos.y);
     }
 }, false);
+
+
+// var waypoint = new Waypoint({
+//   element: document.getElementById('popos-1'),
+//   handler: function(direction) {
+//     notify('I am 20px from the top of the window')
+//   },
+//   offset: 20
+// })
